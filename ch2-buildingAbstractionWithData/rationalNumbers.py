0cb1a073d5654a0a64a1dccdb6ace7db73b0b188
Eason0210 <aqua0210@163.com>
2022-01-01 18:20:07 +0800

    Add rational numbers

---
+++ ch2-buildingAbstractionWithData/rationalNumbers.py
@@ -0,0 +1,27 @@
+"""Rational Numbers such as 1/3 or 17/29."""
+
+from math import gcd
+
+def rational(n, d):
+    g = gcd(n, d)
+    return [n // g, d // g]
+
+def numer(x):
+    return x[0]
+
+def denom(x):
+    return x[1]
+
+def add_rationals(x, y):
+    nx, dx = numer(x), denom(x)
+    ny, dy = numer(y), denom(y)
+    return rational(nx * dy + ny * dx, dx * dy)
+
+def mul_rationals(x, y):
+    return rational(numer(x) * numer(y), denom(x) * denom(y))
+
+def print_rational(x):
+    print(numer(x), '/', denom(x))
+
+def rationals_are_equal(x, y):
+    return numer(x) * denom(y) == numer(y) * denom(x)
