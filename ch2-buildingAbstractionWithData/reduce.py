22eeb153947b4b7f84e4d50af670e10f17b14d23
Eason0210 <aqua0210@163.com>
2022-01-02 00:10:20 +0800

    Add reduce.py

---
+++ ch2-buildingAbstractionWithData/reduce.py
@@ -0,0 +1,38 @@
+"""Reducing a sequence to a value."""
+from operator import add, mul, truediv
+
+def divide_all(n, ds):
+    try:
+        return reduce(truediv, ds, n)
+    except ZeroDivisionError:
+        return float('inf')
+
+def reduce(f, s, initial):
+    """Combine elements of s pairwise using f, starting with initial.
+    E.G., reduce(mul, [2, 4, 8], 1) is equivalent to mul(mul(mul(1,2), 4), 8).
+
+    >>> reduce(mul, [2, 4, 8], 1)
+    64
+    >>> reduce(add, [1, 2, 3, 4], 0)
+    10
+    """
+
+    for x in s:
+        initial = f(initial, x)
+    return initial
+
+def reduce(f, s, initial):
+    """Combine elements of s pairwise using f, starting with initial.
+    E.G., reduce(mul, [2, 4, 8], 1) is equivalent to mul(mul(mul(1,2), 4), 8).
+
+    >>> reduce(mul, [2, 4, 8], 1)
+    64
+    >>> reduce(add, [1, 2, 3, 4], 0)
+    10
+    """
+
+    if not s:
+        return initial
+    else:
+        first, rest = s[0], s[1:]
+        return reduce(f, rest, f(initial, first))
