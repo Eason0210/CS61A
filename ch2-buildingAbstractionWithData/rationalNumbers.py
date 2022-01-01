27b980d33644c6f5ad88093f020c46bb7202022d
Eason0210 <aqua0210@163.com>
2022-01-01 20:51:25 +0800

    Refactoring Rational arithmetic

--- ch2-buildingAbstractionWithData/rationalNumbers.py
+++ ch2-buildingAbstractionWithData/rationalNumbers.py
@@ -2,26 +2,37 @@
 
 from math import gcd
 
-def rational(n, d):
-    g = gcd(n, d)
-    return [n // g, d // g]
-
-def numer(x):
-    return x[0]
-
-def denom(x):
-    return x[1]
+# Rational arithmetic
 
 def add_rationals(x, y):
+    """Add rational numbers x and y."""
     nx, dx = numer(x), denom(x)
     ny, dy = numer(y), denom(y)
     return rational(nx * dy + ny * dx, dx * dy)
 
 def mul_rationals(x, y):
+    """Multiply rational numbers x and y."""
     return rational(numer(x) * numer(y), denom(x) * denom(y))
 
+def rationals_are_equal(x, y):
+    """Return whether rational numbers x and y are equal."""
+    return numer(x) * denom(y) == numer(y) * denom(x)
+
 def print_rational(x):
+    """Print rational x."""
     print(numer(x), '/', denom(x))
 
-def rationals_are_equal(x, y):
-    return numer(x) * denom(y) == numer(y) * denom(x)
+# Constructor and selector
+
+def rational(n, d):
+    """Construct a rational number x that represents N/D"""
+    g = gcd(n, d)
+    return [n // g, d // g]
+
+def numer(x):
+    """Return the numerator of rational number x."""
+    return x[0]
+
+def denom(x):
+    """Return the denominator of rational number x."""
+    return x[1]
