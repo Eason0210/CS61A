33d083cde1eb72a6433f41fa9927bb132944ed74
Eason0210 <aqua0210@163.com>
2022-01-01 20:36:01 +0800

    Add list comprehensions

--- ch2-buildingAbstractionWithData/forStatements.py
+++ ch2-buildingAbstractionWithData/forStatements.py
@@ -48,3 +48,11 @@ def sum_below(n):
 def cheer():
     for _ in range(3):
         print('Go Bears!')
+
+# list comprehensions
+
+odds = [1, 3, 5, 7, 9]
+result = [x+1 for x in odds if 25 % x == 0]
+
+def divisors(n):
+    return [1] + [x for x in range(2, n) if n % x ==0]
