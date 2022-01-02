d1dea4a7789ee22878cba4ba8face19fb2280e5b
Eason0210 <aqua0210@163.com>
2022-01-02 11:59:50 +0800

    Add mutableFunction.py

---
+++ ch2-buildingAbstractionWithData/mutableFunction.py
@@ -0,0 +1,10 @@
+"""Mubable Functions."""
+
+def make_withdraw(balance):
+    def withdraw(amount):
+        nonlocal balance
+        if amount > balance:
+            return 'Insufficient funds.'
+        balance = balance - amount
+        return balance
+    return withdraw
