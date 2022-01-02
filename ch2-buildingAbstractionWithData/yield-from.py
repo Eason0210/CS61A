32515a8fe8fbb8eb2e39f168ea25cc72756c6281
Eason0210 <aqua0210@163.com>
2022-01-02 13:48:58 +0800

    Add yield from

--- ch2-buildingAbstractionWithData/yield-from.py
+++ ch2-buildingAbstractionWithData/yield-from.py
@@ -14,3 +14,34 @@ def a_then_b(a, b):
     #     yield x
     # for x in b:
     #     yield x
+
+
+def countdown(k):
+    """
+
+    >>> list(countdown(5))
+    [5, 4, 3, 2, 1]
+    """
+    if k > 0:
+        yield k
+        yield from countdown(k-1)
+
+def prefixes(s):
+    """
+
+    >>> list(prefixes('both'))
+    ['b', 'bo', 'bot', 'both']
+    """
+    if s:
+        yield from prefixes(s[:-1])
+        yield s
+
+def substrings(s):
+    """
+
+    >>> list(substrings('tops'))
+    ['t', 'to', 'top', 'tops', 'o', 'op', 'ops', 'p', 'ps', 's']
+    """
+    if s:
+        yield from prefixes(s)
+        yield from substrings(s[1:])
