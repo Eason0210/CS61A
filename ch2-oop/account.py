8bc32aa292bc8ef081748970511883dfb538a9c8
Eason0210 <aqua0210@163.com>
2022-01-02 23:00:56 +0800

    Add ch2-oop/account.py

---
+++ ch2-oop/account.py
@@ -0,0 +1,30 @@
+"""Methods"""
+class Account:
+    """An account has a balance and a holder
+
+    >>> a = Account('John')
+    >>> a.deposit(100)
+    100
+    >>> a.withdraw(90)
+    10
+    >>> a.withdraw(90)
+    'Insufficient funds'
+    >>> a.balance
+    10
+    """
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        """Add amount to balance."""
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Subtract amount from balance if funds are available."""
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
