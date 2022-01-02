ff07daf49f6f826c5f96af9c0fdd6d7cea8a16ea
Eason0210 <aqua0210@163.com>
2022-01-03 00:55:26 +0800

    Add landowners

---
+++ ch2-oop/landOwners.py
@@ -0,0 +1,42 @@
+"""Examples: Objects.
+Instance attributes are found before class attributes;
+class attributes are inherited.
+"""
+
+class Worker:
+    """ Try to test it as below:
+
+    >>> jack = Worker()
+    >>> john = Bourgeoisie()
+    >>> jack.greeting = 'Maam'
+
+    >>> Worker().work()
+    'Sir, I work'
+
+    >>> jack
+    Peon
+
+    >>> jack.work()
+    'Maam, I work'
+
+    >>> john.work()
+    Peon, I work
+    'I gather wealth'
+
+    >>> john.elf.work(john)
+    'Peon, I work'
+    """
+    greeting = 'Sir'
+    def __init__(self) -> None:
+            self.elf = Worker
+    def work(self):
+        return self.greeting + ', I work'
+    def __repr__(self) -> str:
+            return Bourgeoisie.greeting
+
+
+class Bourgeoisie(Worker):
+    greeting = 'Peon'
+    def work(self):
+        print(Worker.work(self))
+        return 'I gather wealth'
