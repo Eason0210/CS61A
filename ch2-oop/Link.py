d1d7b76c39e4f0983bf30e6213b2d14ace5d2775
Eason0210 <aqua0210@163.com>
2022-01-03 11:06:27 +0800

    Add Link List

---
+++ ch2-oop/Link.py
@@ -0,0 +1,17 @@
+"""Linked List"""
+
+# help(isinstance): Return whether an object is an instance of a class or of a subclass thereof.
+
+class Link:
+    empty = ()
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
