ac213a189478e0d1939a0485c5b2c0d1d42c7ac8
Eason0210 <aqua0210@163.com>
2022-01-03 11:28:34 +0800

    implement second and other useful functions

--- ch2-oop/Link.py
+++ ch2-oop/Link.py
@@ -9,9 +9,27 @@ class Link:
         self.first = first
         self.rest = rest
 
+    def __getitem__(self, i):
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
+
+    def __len__(self):
+        return 1 + len(self.rest)
+
     def __repr__(self):
         if self.rest:
-            rest_repr = ', ' + repr(self.rest)
+            rest_str = ', ' + repr(self.rest)
         else:
-            rest_repr = ''
-        return 'Link(' + repr(self.first) + rest_repr + ')'
+            rest_str = ''
+        # return 'Link(' + repr(self.first) + rest_str + ')'
+        return 'Link({0}{1})'.format(self.first, rest_str)
+
+    @property
+    def second(self):
+        return self.rest.first
+
+    @second.setter
+    def second(self, value):
+        self.rest.first = value
