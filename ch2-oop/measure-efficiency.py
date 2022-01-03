ba55547bfaa747b11aef91f5c23fadb400d0f8a9
Eason0210 <aqua0210@163.com>
2022-01-03 14:46:39 +0800

    Add memoization

--- ch2-oop/measure-efficiency.py
+++ ch2-oop/measure-efficiency.py
@@ -1,5 +1,20 @@
 """Measuring efficiency."""
 
+""" Test case as below:
+>>> fib(30)
+>>> fib = count(fib)
+>>> counted_fib = fib
+
+>>> fib = memo(fib)
+>>> fib = count(fib)
+>>> fib(30)
+832040
+>>> fib.call_count
+59
+>>> counted_fib.call_count
+31
+"""
+
 def fib(n):
     if n == 0 or n == 1:
         return n
@@ -12,3 +27,11 @@ def count(f):
         return f(n)
     counted.call_count = 0
     return counted
+
+def memo(f):
+    cache = {}
+    def memoized(n):
+        if n not in cache:
+            cache[n] = f(n)
+        return cache[n]
+    return memoized
